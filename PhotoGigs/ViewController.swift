3bbe1755526f972599558c922e07436cc4efc6df
eatikiadm <eatikiadm@eatikiadms-macbook-air.local>
2018-08-17 12:21:37 +0700

    Initial Commit

---
+++ PhotoGigs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoGigs
+//
+//  Created by eatikiadm on 17/08/18.
+//  Copyright © 2018 eatikiadm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
